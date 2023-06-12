a32116b047d09b683a944bd2c00500bc1c6b8adc
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-06-11 23:50:16 -0700

    added contents to webpack.config

--- client/src-sw.js
+++ client/src-sw.js
@@ -24,7 +24,34 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
+//registerRoute= Workbox tool= directs where request should go
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+//"If there is a request for 'style', 'script', or 'worker'...""
+({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  //"...check if we have a version in the cache. If so, use it but also go get the latest from the network and update the cache for next time.
+  //...If it's not in the cache, fetch it from the network. Keep up to 60 of these items in the cache, and remove them if they're older than 30 days."
+  //Anology: Imagine if you asked your helper (service worker) to get a toy from your toy box(network request). 
+    //But to save time, instead of getting the toy right away, your helper gives you an old toy that you played before (that's the "stale" part) 
+    //while it goes to find the toy you asked for in the toy box (that's the "revalidate" part). So, you get to play with the old toy immediately, while your helper is fetching the new toy. That way, you don't have to wait.  
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      //This is a plugin that ensures only certain HTTP response statuses are cached(0, or 200)
+        //HTTP status of 200 means the request was successful, 0 means the request was fulfilled by a service worker.
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      //"...keep up to 60 of these items in the cache, and remove them if they're older than 30 days."
+        //This plugin limits the number of entries in the cache and how long they stay there
+      new ExpirationPlugin({
+        //maximum number of responses that can be stored in the cache. 
+        maxEntries: 60,
+        //cache storage time limit= 30days = conversions(30days*(24hrs/day)*(60min/hr)*(60secs/min))
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,13 +18,73 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //"Create a new HTML plugin"
+      // HtmlWebpackPlugin= generates our html file and injects our bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      //"Inject Manifest plugin"
+      //InjectManifest= injects our custom servie worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      //"PWA Manifest plugin"
+      //WebpackPwaManifest= creates a manifest.json file.
+      //a configuration file that provides information about the app in a JSON text file and helps devices understand and adapt to various device configurations and platforms
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            //This is an array of pixel sizes. This tells the plugin to resize the icon to these dimensions so will have the same icon but in different sizes. Ensures that icon looks good on screens of different resolutions.
+            sizes: [96, 128, 192, 256, 384, 512],
+            //tells the plugin where to output the resized icon images. 
+            //path.join is another function in Node.js that joins the given path segments. 
+              //In this case, it joins 'assets' and 'icons' to form 'assets/icons'. So resized icon images will be put into an 'assets/icons' directory in the output directory of the webpack build.
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
-      rules: [
         
-      ],
+        rules: [
+          //Rule 1
+          // CSS Loaders
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+
+          //Rule 2
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            // Use babel-loader in order to use ES6.
+            use: {
+
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              },
+
+            },
+          },
+
+        ]    
     },
+
   };
 };
